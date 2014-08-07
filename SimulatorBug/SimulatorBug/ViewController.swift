ec7bc83c0350290297ff511b305b87ddb9acf18f
Nick Snyder <nsnyder@linkedin.com>
2014-08-07 16:58:38 -0700

    SimulatorBug

---
+++ SimulatorBug/SimulatorBug/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimulatorBug
+//
+//  Created by Nick Snyder on 8/7/14.
+//  Copyright (c) 2014 LinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.redColor()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
